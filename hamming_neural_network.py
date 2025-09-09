14d7ef59da43fc6b2a39337385b18eda0ec19bf0
Diego Miguel Arebalo <diego.arebalo@liotgroup.io>
2025-09-09 15:58:39 -0300

    inputs by terminal

--- hamming_neural_network.py
+++ hamming_neural_network.py
@@ -2,20 +2,24 @@ import numpy as np
 import hamming_shapes as hs
 import sys
 
+
+
 # 1) Crear red para tamaño N
-N = int(sys.argv[2]) 
+
+N                   = int(input("ingrese dimension de la tabla:"))
+noise               = float(input("ingrese nivel de ruido (0 a 1): "))
+figura              = int(input("ingrese figura prototipo:"))
 net, labels, protos = hs.make_network(N)  # protos: [5, N, N] en binario (0/1)
 
-# 2) Clasificar una matriz binaria NxN (0/1)
-x = protos[int(sys.argv[1])]                     # por ejemplo, el círculo “limpio”
+x = protos[figura]
 # 3) Probar con ruido
-x_noisy = hs.add_noise(x, flip_prob=0.10)
+x_noisy = hs.add_noise(x, noise)
 # prediccion normal
 print("prediccion con ruido:") 
 print(net.predict(x_noisy))                       # -> 'circulo'
 print("prediccion con ruido y rotacion:")
 # prediccion con rotacion
-rotada = np.rot90(x_noisy, k=4)
+rotada = np.rot90(x_noisy, k=2)
 print(net.predict(rotada))              # -> 'circulo' (si no, probar con --no-maxnet)
 
 # pred, scores = net.predict(x_noisy, return_scores=True)
